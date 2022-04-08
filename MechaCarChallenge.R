5fc62055f3ec54472aa1f900a7173375b689ca9a
KyHicks <kyliehicks76@yahoo.com>
2022-04-07 22:12:10 -0500

    Add deliverable 2 to challenge and update readme file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,14 @@ lin_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_c
 
 # Use the summary() function to find p-value and r-squared value
 summary(lin_model)
+
+# Deliverable 2
+
+# Import and read Suspension_coil.csv 
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a total_summary dataframe using summarize()
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Create summary data frame grouped by lot
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+
+! [This is an image] ()
+* The two variables/coefficients that provided a non-random amount of variance to the mpg values in the dataset were "vehicle_length" and "ground_clearance".
+
+* The slope of the linear model is not considered to be zero because the p-value (5.35e-11)  was much lower than the significance level.  This shows that the null bypothesis in not true.
+
+*  The linear model does predict mpg of MechaCar prototypes effectively.  Because the r-squared value is 0.7149 meaning that there is a 71.5% chance that future data will fit this linear model.
